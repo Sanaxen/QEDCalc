d1d195b33019fb388c9e953c366e44b47c897560
Sanaxen <bccatfalcon@gmail.com>
2026-08-25 19:11:05 +0900

    Export safe aligned Markdown math formatter

--- qedcalc/reporting/__init__.py
+++ qedcalc/reporting/__init__.py
@@ -1,5 +1,6 @@
 """Reporting helpers for QEDCalc generated calculation records."""
 
 from .markdown_math_safe import format_markdown_math
+from .markdown_math_aligned import format_markdown_math_aligned
 
-__all__ = ["format_markdown_math"]
+__all__ = ["format_markdown_math", "format_markdown_math_aligned"]
