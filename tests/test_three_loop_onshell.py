4d0a2fefe60b0c5f200a9bb2fff50c39fa340f34
Sanaxen <bccatfalcon@gmail.com>
2026-08-30 16:43:49 +0900

    Validate finite-q p-q basis reduction

--- tests/test_three_loop_onshell.py
+++ tests/test_three_loop_onshell.py
@@ -1,6 +1,11 @@
 import sympy as sp
 
-from three_loop import apply_finite_q_onshell, finite_q_onshell_substitutions
+from three_loop import (
+    apply_finite_q_onshell,
+    finite_q_onshell_substitutions,
+    finite_q_pq_basis_substitutions,
+    rewrite_to_pq_external_basis,
+)
 
 
 def test_finite_q_onshell_relations_preserve_z_dependence():
@@ -31,3 +36,31 @@ def test_finite_q_onshell_reduction_removes_only_external_invariants():
     )
     assert reduced.after_scalar_product_atoms == ("SP__k__p",)
     assert reduced.expression.has(z)
+
+
+def test_pq_basis_substitutions_are_exact_pprime_equals_p_plus_q():
+    subs = finite_q_pq_basis_substitutions()
+    assert subs[sp.Symbol("SP__k__p'")] == sp.Symbol("SP__k__p") + sp.Symbol("SP__k__q")
+    assert subs[sp.Symbol("SP__l__p'")] == sp.Symbol("SP__l__p") + sp.Symbol("SP__l__q")
+    assert subs[sp.Symbol("SP__p'__r")] == sp.Symbol("SP__p__r") + sp.Symbol("SP__q__r")
+
+
+def test_pq_basis_reduction_removes_pprime_loop_atoms_without_qzero_limit():
+    expr = (
+        sp.Symbol("SP__k__p'")
+        + sp.Symbol("SP__l__p'")
+        + sp.Symbol("SP__p'__r")
+        + sp.Symbol("SP__k__l")
+    )
+    reduced = rewrite_to_pq_external_basis(expr)
+    expected = (
+        sp.Symbol("SP__k__p") + sp.Symbol("SP__k__q")
+        + sp.Symbol("SP__l__p") + sp.Symbol("SP__l__q")
+        + sp.Symbol("SP__p__r") + sp.Symbol("SP__q__r")
+        + sp.Symbol("SP__k__l")
+    )
+    assert sp.expand(reduced.expression - expected) == 0
+    assert not any("p'" in name for name in reduced.after_scalar_product_atoms)
+    assert {"SP__k__q", "SP__l__q", "SP__q__r"}.issubset(
+        set(reduced.after_scalar_product_atoms)
+    )
