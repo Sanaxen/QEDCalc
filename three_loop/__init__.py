6adfc8c2e724d1cad98e41bc84c7b732de2c411e
Sanaxen <bccatfalcon@gmail.com>
2026-08-30 12:05:25 +0900

    Expose three-loop magnetic projector API

--- three_loop/__init__.py
+++ three_loop/__init__.py
@@ -1,10 +1,18 @@
 """Three-loop stages 1-3 foundation."""
 from .registry import PhotonEdge, ThreeLoopRegistry, ThreeLoopTopology
 from .amplitude import Factor, OrderedAmplitude, build_ordered_amplitude
+from .projector import (
+    MagneticProjector,
+    three_loop_magnetic_projector,
+    project_f2_from_reduced_current,
+    schwinger_gordon_checkpoint,
+)
 from .divergence import DivergentSubgraph, discover_candidate_subgraphs
 
 __all__ = [
     "PhotonEdge", "ThreeLoopRegistry", "ThreeLoopTopology",
     "Factor", "OrderedAmplitude", "build_ordered_amplitude",
+    "MagneticProjector", "three_loop_magnetic_projector",
+    "project_f2_from_reduced_current", "schwinger_gordon_checkpoint",
     "DivergentSubgraph", "discover_candidate_subgraphs",
 ]
