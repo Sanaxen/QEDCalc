1527ca256e265acf5dfc7955e8a4506124c22d43
Sanaxen <bccatfalcon@gmail.com>
2026-09-15 19:07:24 +0900

    Extend existing-family reuse audit through Q10

--- examples/three_loop_next_existing_family_reuse_audit.py
+++ examples/three_loop_next_existing_family_reuse_audit.py
@@ -22,9 +22,10 @@ CONFIRMED_QUENCHED_IDS = {
     "Q07", "Q47",
     "Q08", "Q48",
     "Q09", "Q49",
+    "Q10", "Q50",
 }
 CONFIRMED_FAMILY_REPRESENTATIVES = [
-    "Q01", "Q02", "Q03", "Q04", "Q05", "Q06", "Q07", "Q08", "Q09"
+    "Q01", "Q02", "Q03", "Q04", "Q05", "Q06", "Q07", "Q08", "Q09", "Q10"
 ]
 
 
